b20d540c99fa8d00bce8cc4ce4b0ae4fd4a46b4d
Satheesh <shansatech@gmail.com>
2022-07-04 18:23:45 +0530

    initial commit

---
+++ json-methods/script1.js
@@ -0,0 +1,7 @@
+const person = '{"name":"Satheesh","age":27,"Qualification":"BCA"}'
+// const str = JSON.stringify(person)
+const nostr = JSON.parse(person)
+// const nostr = JSON.parse(str)
+console.log(person)
+// console.log(str)
+console.log(nostr)
---
+++ json-methods/script2.js
@@ -0,0 +1,4 @@
+const person = { name: "Satheesh", age: 27, Qualification: "BCA" }
+const str = JSON.stringify(person)
+console.log(person)
+console.log(str)
